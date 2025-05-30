d9575bc2a88b4c7f33bbc1d0d49ac8ca45444932
Hubert Blein <h.blein@meta2i.com>
2025-05-30 20:24:10 +0200

    fix header style + responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { AuthComponent } from './components/auth/auth.component';
 import { AuthRedirectService } from './services/auth-redirect.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, AuthComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
